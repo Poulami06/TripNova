208d4eab5eb29e9887076bb94541033700a236d6
Poulami <haldarpoulami10@gmail.com>
2026-08-29 12:15:45 +0530

    Updated init script for Atlas DB

--- init/index.js
+++ init/index.js
@@ -1,48 +1,43 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config({ path: "../.env" });
+}
+
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
-const Review = require("../models/review.js");
 
+const dbUrl = process.env.ATLASDB_URL;
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/tripnova";
-
-async function main() {
-  await mongoose.connect(MONGO_URL);
-  await Review.deleteMany({});
-  await Listing.updateMany({}, { $set: { reviews: [] } });
-  console.log("All old reviews deleted successfully!");
-  mongoose.connection.close();
-}
-
-main();
-
-main().then(() => {
-    console.log("connected to DB");
-})
-.catch((err) => {
-    console.log(err);
-});
+main()
+    .then(() => {
+        console.log("connected to DB");
+        initDB();
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 const categories = [
-  "Trending",
-  "Iconic Cities",
-  "Mountains",
-  "Castles",
-  "Camping",
-  "Farms",
-  "Arctic"
+    "Trending",
+    "Iconic Cities",
+    "Mountains",
+    "Castles",
+    "Camping",
+    "Farms",
+    "Arctic"
 ];
-
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj, index) => ({...obj, owner:'6a915dad2ff4f11a36c82d5f' , category: categories[index % categories.length]}));
+    initData.data = initData.data.map((obj, index) => ({
+        ...obj,
+        owner: "6a915dad2ff4f11a36c82d5f",
+        category: categories[index % categories.length]
+    }));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
-
-initDB();
